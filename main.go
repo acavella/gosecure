fe4c424877d9ec7d8e9c6037b1265004c2cf1c17
Tony Cavella <9434191+acavella@users.noreply.github.com>
2023-09-12 00:52:12 +0000

    build version function

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"flag"
-	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -25,8 +24,6 @@ var (
 	debugPtr   bool
 )
 
-var Version = "No Version Provided"
-
 func init() {
 
 	flag.BoolVar(&encryptPtr, "e", false, "Encrypt the input data.")
@@ -45,7 +42,7 @@ func init() {
 		log.SetLevel(log.WarnLevel)
 	}
 
-	fmt.Printf("App Version: %s\n", Version)
+	printver()
 
 }
 
---
+++ version.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+var appVersion = "No Version Provided"
+
+func printver() {
+	fmt.Printf("App Version: %s\n", appVersion)
+}
