6845947fe9b27d139406b8a89710d4a4b1fe6066
Tony Cavella <9434191+acavella@users.noreply.github.com>
2023-09-30 16:11:05 +0000

    refactor: update version output

--- version.go
+++ version.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 var appVersion = "No Version Provided"
+var appBuild = "No Build Provided"
+var appDate = "No Build Date"
 
 func printver() {
-	fmt.Printf("App Version: %s\n", appVersion)
+	fmt.Printf("GoShield %s\n", appVersion)
+	fmt.Printf("Build Number: %s\n", appBuild)
+	fmt.Printf("Build Date: %s\n", appDate)
 }
