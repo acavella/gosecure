574b4ebc83d6c1f7bf026ee7f8adface32b8d3f3
Tony Cavella <9434191+acavella@users.noreply.github.com>
2023-10-03 19:00:39 +0000

    ci: update release workflow add build number variable

--- version.go
+++ version.go
@@ -4,10 +4,8 @@ import "fmt"
 
 var appVersion = "v0.0.0"
 var appBuild = "0000000"
-var appDate = "0/0/00"
 
 func printver() {
 	fmt.Printf("GoShield %s\n", appVersion)
 	fmt.Printf("Build Number: %s\n", appBuild)
-	fmt.Printf("Build Date: %s\n", appDate)
 }
